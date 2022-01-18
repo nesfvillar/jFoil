0089253f8aef0f106d7d0dad46cb7c7603f8f1ae
Nestor Fabian Villar <nesfvillar@gmail.com>
2022-01-18 03:18:07 -0300

    Pep-8 and change of some names

--- jFoil/jFoil.py
+++ jFoil/jFoil.py
@@ -2,35 +2,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+
 class jFoil:
-    """ Jowkousky foil creator.
+    """ Joukowsky foil creator.
 
     Parameters:
 
     a - Radius of the pre-transformation cilinder. Approximately 1/4 of the final chord length.
 
-    bOverA - Proportional to the thickness of the airfoil. Must be between 0 and 1. For airfoils, it is generally close to 0.9.
+    relThickness - Proportional to the thickness of the airfoil. Must be between 0 and 1. For airfoils, it is generally
+    close to 0.9.
 
     beta - Curvature of the foil in degrees.
 
-    [n] - number of samples that make the foil. Default 50. """
+    [n] - number of samples that make the foil. Default 100. """
 
-    def __init__(self, a, bOverA, beta, n=50):
-        if bOverA < 0 or bOverA > 1:
+    def __init__(self, a, relThickness, beta, n=100):
+        if relThickness < 0 or relThickness > 1:
             raise ValueError(
-                "Bad value of airfoil thickness (bOverA must be between 0 and 1)")
+                "Bad value of airfoil thickness (relative thickness must be between 0 and 1)")
 
         self.a = a
-        self.bOverA = bOverA
+        self.relThickness = relThickness
         self.beta = np.deg2rad(beta)
 
         self.theta = np.linspace(0, 2*np.pi, n)
-        self.cilinder = self.a * (np.exp(1j*self.theta) - self.bOverA +
+        self.cilinder = self.a * (np.exp(1j*self.theta) - self.relThickness +
                                   np.exp(1j*self.beta))
 
-        self.foil = joukowskyTransform(self.cilinder, self.a * self.bOverA)
+        self.foil = joukowskyTransform(
+            self.cilinder, self.a * self.relThickness)
 
     def plotFoil(self, title='', grid=False):
+        """  plotFoil """
         plt.plot(np.real(self.foil), np.imag(self.foil))
         plt.title(title)
         plt.axis("scaled")
@@ -39,14 +43,15 @@ class jFoil:
         plt.show()
 
     def saveFoil(self, filename='foil.csv'):
+        """ saveFoil """
         np.savetxt(filename, self.foil, fmt='%f;%f', header='x;y', comments='')
 
-    def currentLines(self, alpha=0):
-        # TODO add current lines plot
-        return
+    def plotCurrent(self, alpha=0, n=100):
+        """ plotCurrent """
+        alpha = np.deg2rad(alpha)
 
     def plotcL(self, minAlpha=-5, maxAlpha=5, n=50):
-        # TODO plot cL as a function of alpha
+        """ plotcL """
         minAlpha = np.deg2rad(minAlpha)
         maxAlpha = np.deg2rad(maxAlpha)
 
@@ -65,7 +70,8 @@ def joukowskyTransform(z, b):
 
 
 def main():
-    # TODO add input to use the command line to create airfoils
+    foil = jFoil(1, 0.9, 5)
+    foil.plotCurrent()
     return
 
 
