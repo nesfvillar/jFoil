dd545588d0dd27e64787792bd31e111da0dc1f81
Nestor Fabian Villar <nesfvillar@gmail.com>
2022-07-20 00:01:22 -0300

    Removed extra work for the class (calculating cl). Created a base airfoil class.

--- jFoil/jFoil.py
+++ jFoil/jFoil.py
@@ -1,35 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-class jFoil:
-    """ Joukowsky foil creator.
-
-    Parameters:
-
-    a - Radius of the pre-transformation cilinder. Approximately 1/4 of the final chord length.
-
-    relThickness - Proportional to the thickness of the airfoil. Must be between 0 and 1. For airfoils, it is generally close to 0.9.
-
-    beta - Curvature of the foil in degrees.
-
-    [n] - number of samples that make the foil. Default 100. """
-
-    def __init__(self, a, relThickness, beta, n=100):
-        if relThickness < 0 or relThickness > 1:
-            raise ValueError(
-                "Bad value of airfoil thickness (relative thickness must be between 0 and 1)")
-
-        self.a = a
-        self.relThickness = relThickness
-        self.beta = np.deg2rad(beta)
-
-        self._theta = np.linspace(0, 2*np.pi, n)
-        self.cilinder = self.a * (np.exp(1j*self._theta) - self.relThickness +
-                                  np.exp(1j*self.beta))
-
-        self.foil = _joukowskyTransform(
-            self.cilinder, self.a * self.relThickness)
+class Airfoil:
+    def __init__(self, foil):
+        self.foil = foil
 
     def plotFoil(self, title='', grid=False):
         """  plotFoil """
@@ -44,56 +18,31 @@ class jFoil:
         """ saveFoil """
         np.savetxt(filename, self.foil, fmt='%f;%f', header='x;y', comments='')
 
-    def _plotCurrent(self, alpha=0, n=100):
-        """ (WIP) plotCurrent """
-        xlim, ylim = 3, 3
-        alpha = np.deg2rad(alpha)
 
-        velocity = 1
-        circulation = 4 * np.pi * self.a * velocity * np.sin(alpha + self.beta)
+class JFoil(Airfoil):
+    """ Joukowsky foil creator.
 
-        x, y = np.mgrid[-xlim:xlim:1j*n, -ylim:ylim:1j*n]
+    Parameters:
 
-        z = x + 1j*y + self.a * self.relThickness - \
-            self.a * np.exp(1j*self.beta)
+    a - Radius of the pre-transformation cilinder. Approximately 1/4 of the final chord length.
 
-        z *= np.abs(z) >= self.a
+    relThickness - Proportional to the thickness of the airfoil. Must be between 0 and 1. For airfoils, it is generally close to 0.9.
 
-        f = _uniformCurrent(z, velocity, alpha) + \
-            _dipole(z, velocity, self.a) - _rotor(z, circulation)
+    beta - Curvature of the foil in degrees.
 
-        psi = np.imag(f)
+    [n] - number of samples that make the foil. Default 100. """
 
-        plt.contour(x, y, psi, levels=50)
-        plt.axis("scaled")
-        plt.show()
+    def __init__(self, a, relThickness, beta, n=100):
+        assert relThickness < 0 and relThickness > 1
 
-    def plotCl(self, minAlpha=-5, maxAlpha=5, n=50):
-        """ plotcL """
-        minAlpha = np.deg2rad(minAlpha)
-        maxAlpha = np.deg2rad(maxAlpha)
+        self.a = a
+        self.relThickness = relThickness
+        self.beta = np.deg2rad(beta)
 
-        alpha = np.linspace(minAlpha, maxAlpha, n)
-        cL = 2*np.pi * np.sin(alpha + self.beta)
+        theta = np.linspace(0, 2*np.pi, n)
+        cilinder = self.a * (np.exp(1j*theta) - self.relThickness + np.exp(1j*self.beta))
 
-        plt.plot(np.rad2deg(alpha), cL)
-        plt.xlabel("alpha (°)")
-        plt.ylabel("cL")
-        plt.grid()
-        plt.show()
+        super().__init__(self._joukowskyTransform(self.cilinder, self.a * self.relThickness))
 
-
-def _joukowskyTransform(z, b):
-    return z + b**2 / z
-
-
-def _uniformCurrent(z, uInf, alpha):
-    return uInf * z * np.exp(1j*alpha)
-
-
-def _dipole(z, uInf, a):
-    return uInf * a**2 / z
-
-
-def _rotor(z, circulation):
-    return -1j / 2*np.pi * circulation * np.log(z)
+    def _joukowskyTransform(z, b):
+        return z + b**2 / z
